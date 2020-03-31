6b524f2052954ecd5cf0958f18374d2d97c8123c
Marcelle <marcelleb@bbd.co.za>
2020-03-31 17:57:39 +0200

    Fixed Address name spacing and moved drop statment

--- AddressTable.sql
+++ AddressTable.sql
@@ -1,12 +1,10 @@
-DROP TABLE IF EXISTS dbo.Address;
-
 CREATE TABLE BloodDonationDB.dbo.Address
 (
     [AddressID] BIGINT NOT NULL IDENTITY,
-    [Complex Name] VARCHAR(50) NOT NULL,
-    [Unit Number] SMALLINT NOT NULL,
-    [Street Name] VARCHAR(50) NOT NULL,
-    [Street Number] SMALLINT NOT NULL,
+    [ComplexName] VARCHAR(50) NOT NULL,
+    [UnitNumber] SMALLINT NOT NULL,
+    [StreetName] VARCHAR(50) NOT NULL,
+    [StreetNumber] SMALLINT NOT NULL,
     [Suburb] VARCHAR(50) NOT NULL,
     [City] VARCHAR(50) NOT NULL,
     [Province] VARCHAR(50) NOT NULL,
