95394ec77dd7e97e0efc5592c1da8719c178627e
Ronaldo <ronaldo@bbd.co.za>
2020-04-08 16:26:30 +0200

    change

--- FacilityInventoryView.sql
+++ FacilityInventoryView.sql
@@ -1,10 +1,7 @@
-create procedure FacilityInventoryView as
-	begin
-		select f.Name as FacilityName,it.Description as ItemType , b.BloodTypeName ,a.Province
-		from Facility f
-		INNER JOIN Inventory i on i.StorageFacilityID=f.FacilityID 
-		INNER JOIN BloodType b on b.BloodTypeID=i.BloodTypeID
-		INNER JOIN ItemType it on it.ItemTypeID=i.ItemTypeID
-		INNER JOIN Address a on a.AddressID=f.AddressID
-	end
-go
+create view FacilityInventoryView as
+select f.Name as FacilityName,it.Description as ItemType , b.BloodTypeName ,a.Province
+from Facility f
+inner join Inventory i on i.StorageFacilityID=f.FacilityID 
+inner join BloodType b on b.BloodTypeID=i.BloodTypeID
+inner join ItemType it on it.ItemTypeID=i.ItemTypeID
+inner join Address a on a.AddressID=f.AddressID
