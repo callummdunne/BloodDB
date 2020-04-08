01d8268670a7e26f5eea5a9aa8d1dedb968433ee
Zanrich <zzanrich@gmail.com>
2020-04-08 13:38:58 +0200

    Update CreateOrderPlacedTable.sql
    Changed HospitalID to facilityID

--- CreateOrderPlacedTable.sql
+++ CreateOrderPlacedTable.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS dbo.OrderPlaced;
 --Create OrderPlaced table
 CREATE TABLE BloodDonationDB.dbo.OrderPlaced(
 [OrderID] BIGINT IDENTITY(1,1) NOT NULL,
-[HospitalID] BIGINT NOT NULL,
+[FacilityID] BIGINT NOT NULL,
 [DatePlaced] DATETIME2(0) NOT NULL,
 [DateDelivered] DATETIME2(0) NOT NULL,
 [OrderStatusID] SMALLINT NOT NULL,
