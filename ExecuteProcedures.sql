66950bbcadd070ee40d21177ae1299871d28194b
Zanrich <zzanrich@gmail.com>
2020-04-08 13:26:31 +0200

    Delete OrdersPlaced Procedures
    Procedures to delete ordersplaced with delivery date x amount of years ago and marked as complete

--- ExecuteProcedures.sql
+++ ExecuteProcedures.sql
@@ -5,3 +5,7 @@ GO
 --Archives Donor when LastDateDonated > 50 years
 EXEC ArchiveDonor;
 GO
+
+--Execute below lines to delete orders that was placed x year ago from today and marked as complete
+EXEC DeleteOrderPlaced @Years = 10; 
+GO
