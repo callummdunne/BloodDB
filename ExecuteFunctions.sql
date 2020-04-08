42e0caea55e59fc1e6c6c9e1be07fc6fd5809935
Zanrich <zzanrich@gmail.com>
2020-04-08 18:54:21 +0200

    Functiontofindordersbydonorname
    Function used to count number of orders per donor name

--- ExecuteFunctions.sql
+++ ExecuteFunctions.sql
@@ -1,3 +1,6 @@
 -- A function that returns count of nr of inventory items with a given order status 
 --change parameter to any order status
 select dbo.InventoryOrderStatus('Completed');
+
+--This function returns the number of orders given a donor name
+select dbo.AmountofOrdersPerDonor('Tia') AS NumberOfOrdersForDonor;
