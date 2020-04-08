d02e20d4499b460901c8a28afea5d9fb932ff93f
chelseajessiman <53180222+chelseajessiman@users.noreply.github.com>
2020-04-08 16:18:43 +0200

    Removed archive procedures

--- ExecuteProcedures.sql
+++ ExecuteProcedures.sql
@@ -5,7 +5,3 @@ GO
 --Execute below lines to delete orders that was placed 1 year ago from today and marked as cancelled
 EXEC DeleteOrderCancelled;
 GO
-
--- A function that returns count of nr of inventory items with a given order status 
---change parameter to any order status
-select dbo.InventoryOrderStatus('Completed');
