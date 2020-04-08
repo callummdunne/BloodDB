7b1cdd932f6f74da9f498a40667f469b0f00b1b8
Zanrich <zzanrich@gmail.com>
2020-04-08 15:00:53 +0200

    Create ImportantOrdersView.sql
    Created a view where one can see the urgent delivery orders, the type of orderser, expected delivery date, and facility with their numbers in order to follow up on deliveries not made yet.

---
+++ ImportantOrdersView.sql
@@ -0,0 +1,25 @@
+CREATE VIEW ImportantDeliveries AS
+SELECT 
+	it.Description AS Description,
+	oi.OrderItemID AS ItemID,
+	op.OrderID AS OrderID,
+	os.Description AS OrderStatus,
+	oi.RequiredDeliveryDate AS ExpectedDeliveryDate,
+	f.Name AS Facility,
+	t.WorkNumber AS Telephone
+FROM OrderPlaced op, OrderItem oi, Inventory i, ItemType it, OrderStatus os, Facility f, Telephone t
+WHERE 
+	it.ItemTypeID = i.ItemTypeID
+AND
+	i.InventoryID = oi.InventoryID
+AND
+	oi.OrderID = op.OrderID
+AND
+	os.OrderStatusID = op.OrderStatusID
+AND
+	f.FacilityID = i.DonationBranchID
+AND
+	f.TelephoneID = t.TelephoneID
+AND 
+	oi.UrgentDelivery = '1';
+GO
