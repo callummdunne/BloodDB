c7fc9328fd8779daa1b50eda9d30d748e476c275
chelseajessiman <53180222+chelseajessiman@users.noreply.github.com>
2020-04-08 16:18:09 +0200

    added function

--- ExecuteProcedures.sql
+++ ExecuteProcedures.sql
@@ -1,11 +1,3 @@
---Execute below lines to run procedure to archive people
-EXEC ArchivePerson;
-GO
-
---Archives Donor when LastDateDonated > 50 years
-EXEC ArchiveDonor;
-GO
-
 --Execute below lines to delete orders that was placed x year ago from today and marked as complete
 EXEC DeleteOrderPlaced @Years = 10; 
 GO
@@ -13,3 +5,7 @@ GO
 --Execute below lines to delete orders that was placed 1 year ago from today and marked as cancelled
 EXEC DeleteOrderCancelled;
 GO
+
+-- A function that returns count of nr of inventory items with a given order status 
+--change parameter to any order status
+select dbo.InventoryOrderStatus('Completed');
